f9e76a963b29e119f0036e7eebab58ef150af193
akshaychavan010101 <akshaychavan010101@gmail.com>
2025-05-14 22:42:05 +0530

    setup: vuejs with tailwind css

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
+</script>
+
+<template>
+  <main>
+    <div class="min-h-screen flex items-center justify-center bg-gray-300">
+      <h1 class="text-4xl font-bold text-blue-600">Tailwind CSS is working 🎉</h1>
+    </div>
+  </main>
+</template>
