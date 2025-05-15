50f15012c966ba794b1d2fd38ff40d461b74161c
akshaychavan010101 <akshaychavan010101@gmail.com>
2025-05-15 11:19:06 +0530

    landing page

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -5,10 +5,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faXmark, faUser, faClock } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faXmark, faUser, faClock)
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
-
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
